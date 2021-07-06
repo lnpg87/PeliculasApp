17ba451a7b42b8e7d8a2be1b149cc1af50a13b8e
Luis Paulino <lnpg87@gmail.com>
2021-07-05 20:34:12 -0400

    Primer Commit

---
+++ src/app/utilidades/rating/rating.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
+})
+export class RatingComponent implements OnInit {
+
+  @Input()
+  maximoRating = 5;
+
+  @Input()
+  ratingSeleccionado = 0;
+
+  maximoRatingArr:number[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.maximoRatingArr = Array(this.maximoRating).fill(0);
+  }
+
+}
